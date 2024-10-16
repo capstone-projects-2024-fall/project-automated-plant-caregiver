336c6b8a843cff8b86f95e0aacfe1dbd19c52442
aidanb3765 <aidan.blaney@temple.edu>
2024-10-16 13:09:45 -0400

    Added two test cases
    Added test case to verify string type and checked what the string contains

--- plantparent/src/helloWorld.test.js
+++ plantparent/src/helloWorld.test.js
@@ -4,3 +4,11 @@ const helloWorld = require('./helloWorld');
 test('returns "Hello, World!"', () => {
   expect(helloWorld()).toBe("Hello, World!");
 });
+
+test('does not return "Goodbye, World!"', () => {
+  expect(helloWorld()).not.toBe("Goodbye, World!");
+});
+
+test('returns a string type', () => {
+  expect(typeof helloWorld()).toBe("string");
+});
