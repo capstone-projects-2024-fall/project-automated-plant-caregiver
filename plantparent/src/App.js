35cfc7b3f23cda66acea66c3618c02e049c6526d
SeaRisk <123013406+SeaRisk@users.noreply.github.com>
2024-11-11 13:17:39 -0500

    Revert "Plant imaging & login (#28)"
    This reverts commit 0cd96f517762dd2f00bfc0f22f36b0dbb3f85ba7.

--- plantparent/src/App.js
+++ plantparent/src/App.js
@@ -6,7 +6,6 @@ import Navbar from './components/navbar';
 import Chat from './components/chat';
 import About from './components/about';
 import Landing from './components/landing';
-import Login from './components/AuthPage';
 import DetailedView from './components/detailedView';
 
 
@@ -22,7 +21,6 @@ function App() {
             <Route path='/landing' element={<Landing />} />
             <Route path='/chat' element={<Chat />} />
             <Route path='/about' element={<About />} />
-            <Route path='/AuthPage' element={<Login />}/>
             <Route path='/detailedView' element={<DetailedView />} />
           </Routes>
       </div>
