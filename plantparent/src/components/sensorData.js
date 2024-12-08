6afe3fa6bbd02e31025ca06a1bca6ec02b14dd45
SeaRisk <123013406+SeaRisk@users.noreply.github.com>
2024-12-08 10:43:44 -0500

    corrected aws folder (#44)
    * corrected aws folder
    
    * corrected aws login

--- plantparent/src/components/sensorData.js
+++ plantparent/src/components/sensorData.js
@@ -1,29 +1,25 @@
-// fetchSensorData.js
-
 const fetchSensorData = async (setSensorData, setError) => {
-    try {
-      const response = await fetch('https://zxk89h80gg.execute-api.us-east-1.amazonaws.com/dev/data', {
-        method: 'GET',  
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  try {
+      const response = await fetch('https://so7bglvkza.execute-api.us-east-1.amazonaws.com/dev/sensorDataV2?mostRecent=true', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+          },
       });
-  
+
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+
       const data = await response.json();
-      console.log('Received data:', data);
-  
-      // Assuming the most recent data is at index 0
-      const latestData = data[0];  // Get the first element of the array
-      setSensorData(latestData);  // Set only the latest data
-    } catch (err) {
-      console.error("Failed to fetch sensor data:", err);
+      console.log('Most recent data:', data);
+
+      // Set the most recent sensor data
+      setSensorData(data); 
+  } catch (err) {
+      console.error('Failed to fetch sensor data:', err);
       setError('Failed to load sensor data.');
-    }
-  };
-  
-  export default fetchSensorData;
-  
+  }
+};
+
+export default fetchSensorData;
